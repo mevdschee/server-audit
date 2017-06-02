c7c89ba4718eea46f3f90b686d0a6f6c2382aeac
Maurits van der Schee <maurits@vdschee.nl>
2017-06-02 13:00:02 +0200

    small fix

--- debian/access/ssh_daemon_config.sh
+++ debian/access/ssh_daemon_config.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-echo "/etc/ssh/sshd_config:"
 if [ -e "/etc/ssh/sshd_config" ]; then
-  cat /etc/ssh/sshd_config \
+  echo "/etc/ssh/sshd_config:" && cat /etc/ssh/sshd_config \
   | grep -v "^\s*#" \
   | grep "ChallengeResponseAuthentication\|PasswordAuthentication\|PermitRootLogin" \
   | diff -b - <(printf "ChallengeResponseAuthentication no\nPasswordAuthentication no\nPermitRootLogin no\n") \
   | grep "^<\|^>" \
   | sort -d
 else
-  echo "File does not exist"
+  echo "File '/etc/ssh/sshd_config' does not exist"
 fi
