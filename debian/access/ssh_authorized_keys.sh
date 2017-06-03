be357f7fd5fbb5e0e63d8019c646b62da549f382
Maurits van der Schee <maurits@vdschee.nl>
2017-06-03 09:45:01 +0200

    limit output

--- debian/access/ssh_authorized_keys.sh
+++ debian/access/ssh_authorized_keys.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 for X in $(cut -f6 -d ':' /etc/passwd |sort |uniq); do
     if [ -s "${X}/.ssh/authorized_keys" ]; then
-        echo "### ${X}: "
-        cat "${X}/.ssh/authorized_keys"
-        echo ""
+        echo "${X}:"
+        cat "${X}/.ssh/authorized_keys" \
+        | grep -v "^\s*#" \
+        | cut -d' ' -f1,3
     fi
 done
